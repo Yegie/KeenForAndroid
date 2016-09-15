70c3b844c58e82609b01eda737ee704c95fff140
Yegie <yegres.vestlam@gmail.com>
2016-09-15 13:09:32 -0400

    added non functioning menu buttons

--- app/src/main/java/org/yegie/keenforandroid/MenuActivity.java
+++ app/src/main/java/org/yegie/keenforandroid/MenuActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by Sergey on 7/17/2016.
@@ -25,7 +27,26 @@ public class MenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Button startButton=(Button) findViewById(R.id.button_start);
+        Button startButton= (Button) findViewById(R.id.button_start);
+
+        //set up the size button
+        Spinner sizeButton= (Spinner) findViewById(R.id.button_size);
+
+        ArrayAdapter<CharSequence> sizeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.size_list, R.layout.menu_spinner_layout);
+        sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeButton.setAdapter(sizeAdapter);
+
+
+        //set up the size button
+        Spinner diffButton= (Spinner) findViewById(R.id.button_diff);
+        ArrayAdapter<CharSequence> diffAdapter = ArrayAdapter.createFromResource(this,
+                R.array.diff_list, R.layout.menu_spinner_layout);
+        diffAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        diffButton.setAdapter(diffAdapter);
+
+
+        Button multButton=  (Button) findViewById(R.id.button_mult);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
